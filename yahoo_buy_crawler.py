9fb5f81599f2412105409344de537445e990eb2a
Cicilia Lee <aacs0130@gmail.com>
2018-08-10 11:05:37 +0800

    Get category id to name map

--- yahoo_buy_crawler.py
+++ yahoo_buy_crawler.py
@@ -4,34 +4,41 @@ import re
 import selenium
 import csv
 import pandas as pd
-import requests as rs
+import requests as r
 from bs4 import BeautifulSoup as bs 
 
-def get_data_from_url(yahoo_url):
-    df = ''
-
-    return df
-
-def export_to_file(data, out_filename, subId):
+def export_to_file(data, out_filename):
 
 
     return
 
-def query_from_cmd(yahoo_url, subId):
+def query_from_cmd(yahoo_url):
     #subId = [1,999]
-    df = get_data_from_url(yahoo_url)
+    #data_dict.keys()
+    column_name = ["timestamp", "name", "price", "category_id", "category_name"]
+    data_dict = {}
+    cat_id_name_map = {}
 
-    return df
+    #Load URL
+    content = json.loads(r.get(yahoo_url).text, encoding="utf-8")
+
+    #Get (hpp) category id name map
+    for i in content['billboard']['tabs']:
+        cat_id_name_map[i["hpp"]] = i["label"]
+    for i in content['billboard']['othertab']:
+        cat_id_name_map[i["hpp"]] = i["label"]
+
+    print "cat_id_name_map:\n%s" %cat_id_name_map
+
+    return data_dict
 
 def main():
-    yahoo_url = ''
+    yahoo_url = 'https://tw.buy.yahoo.com/catalog/ajax/recmdHotNew'
     out_filename = ''
-    input_line = ''
-    subId = 1
 
     #while(input_line):
-    data = query_from_cmd(yahoo_url, subId)
-    export_to_file(data, out_filename, subId)
+    data = query_from_cmd(yahoo_url)
+    export_to_file(data, out_filename)
 
 
 if __name__ == '__main__':
